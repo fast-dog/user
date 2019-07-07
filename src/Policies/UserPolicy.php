5718a63875b0e9dbf98aab6fe12be885b7fa0d5a
Andrey Martynov <d.g.dev482@gmail.com>
2019-07-07 15:40:07 +0500

    fix policies

--- src/Policies/UserPolicy.php
+++ src/Policies/UserPolicy.php
@@ -8,7 +8,7 @@ use Illuminate\Auth\Access\HandlesAuthorization;
 
 /**
  * Class UserPolicy
- * 
+ *
  * @package App\Policies
  * @version 0.2.0
  * @author Андрей Мартынов <d.g.dev482@gmail.com>
@@ -27,10 +27,54 @@ class UserPolicy
         //
     }
 
-    public function before(User $user, $ability)
+    /**
+     * @param User $user
+     * @param $ability
+     * @return bool
+     */
+    public function reorder(User $user, $ability): bool
     {
-        if ($user->{User::TYPE} === User::USER_TYPE_ADMIN) {
-            return true;
-        }
+        return ($user->{User::TYPE} === User::USER_TYPE_ADMIN);
+    }
+
+    /**
+     * @param User $user
+     * @param $ability
+     * @return bool
+     */
+    public function create(User $user, $ability): bool
+    {
+        return ($user->{User::TYPE} === User::USER_TYPE_ADMIN);
+    }
+
+    /**
+     * @param User $user
+     * @param $ability
+     * @return bool
+     */
+    public function delete(User $user, $ability): bool
+    {
+        return ($user->{User::TYPE} === User::USER_TYPE_ADMIN);
+    }
+
+    /**
+     * @param User $user
+     * @param $ability
+     * @return bool
+     */
+    public function update(User $user, $ability): bool
+    {
+        return ($user->{User::TYPE} === User::USER_TYPE_ADMIN);
+
+    }
+
+    /**
+     * @param User $user
+     * @param $ability
+     * @return bool
+     */
+    public function before(User $user, $ability): bool
+    {
+        return ($user->{User::TYPE} === User::USER_TYPE_ADMIN);
     }
 }
