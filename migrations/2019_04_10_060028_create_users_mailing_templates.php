542cc413b097332e069e9ad88004e73ea797d77f
dg <d.g.dev482@gmail.com>
2019-07-28 22:54:44 +0500

    fix migration

--- migrations/2019_04_10_060028_create_users_mailing_templates.php
+++ migrations/2019_04_10_060028_create_users_mailing_templates.php
@@ -1,6 +1,5 @@
 <?php
 
-use FastDog\Core\Properties\BaseProperties;
 use FastDog\User\Models\UserMailingTemplates;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
@@ -28,33 +27,6 @@ class CreateUsersMailingTemplates extends Migration
             });
 
             DB::statement("ALTER TABLE `users_mailing_templates` comment 'Шаблоны рассылок'");
-
-            /** @var UserMailingTemplates $firstTemplate */
-
-            $firstTemplate = UserMailingTemplates::create([
-                UserMailingTemplates::NAME => trans('user::mailing.template.new.name'),
-                UserMailingTemplates::TEXT => trans('user::mailing.template.new.html'),
-                UserMailingTemplates::STATE => UserMailingTemplates::STATE_PUBLISHED,
-                UserMailingTemplates::SITE_ID => '001',
-            ]);
-
-            /** @var \Illuminate\Support\Collection $properties */
-            $properties = $firstTemplate->properties();
-
-            $saveProperties = [];
-            $fillProperties = [
-                'FROM_ADDRESS' => config('mail.from.address'),
-                'FROM_NAME' => config('mail.from.name'),
-            ];
-            $properties->each(function ($property) use (&$saveProperties, $fillProperties) {
-                if (isset($fillProperties[$property[BaseProperties::ALIAS]])) {
-                    $property[BaseProperties::VALUE] = $fillProperties[$property[BaseProperties::ALIAS]];
-                    $property['show'] = true;// <- активное свойство
-                    $saveProperties[] = $property;
-                }
-            });
-
-            $firstTemplate->storeProperties(collect($saveProperties));
         }
     }
 
